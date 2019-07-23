72d016ea9c725ae32efedd62ffc53b999a492360
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-23 16:07:35 +0700

    nav + event + property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {RecipeModel} from '../recipe.model';
 
 @Component({
@@ -7,15 +7,19 @@ import {RecipeModel} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<RecipeModel>();
   recipes: RecipeModel[] = [
-    new RecipeModel('A Test Recipe', 'This is  a simple test', 'https://www.dinneratthezoo.com/wp-content/uploads/2017/10/firecracker-chicken-1.jpg'),
-    new RecipeModel('A Test Recipe', 'This is  a simple test', 'https://www.dinneratthezoo.com/wp-content/uploads/2017/10/firecracker-chicken-1.jpg')
+    new RecipeModel('Recipe 1', 'Shrimp', 'https://www.dinneratthezoo.com/wp-content/uploads/2018/04/garlic-butter-shrimp-3.jpg'),
+    new RecipeModel('Recipe 2', 'Steak', 'https://therecipecritic.com/wp-content/uploads/2017/10/skilletgarlicbutterherbsteakpotatoes-1-of-1.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: RecipeModel) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {IngredientModel} from '../../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<IngredientModel>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const newIngredient = new IngredientModel(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
