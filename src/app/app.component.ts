0088b5d5344ecd2fa60d2cf973b6e0063ddd322b
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-23 14:15:17 +0700

    finish feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ServerModel} from './model/server.model';
+import {ServerManagerModel} from './model/server-manager.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udemy-angular8';
+  serverElements = [new ServerModel('server', 'Vista', 'Beta'),
+    new ServerModel('server', 'El Capital', 'Official'),
+    new ServerModel('blueprint', '12.04 LTS', 'Official')];
+
+  addServer(server: ServerManagerModel) {
+    this.serverElements.push({
+      type: 'server',
+      name: server.name,
+      content: server.content
+    });
+  }
+
+  addServerBluePrint(server: ServerManagerModel) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: server.name,
+      content: server.content
+    });
+  }
 }
---
+++ src/app/model/server.model.ts
@@ -0,0 +1,11 @@
+export class ServerModel {
+  type: string;
+  name: string;
+  content: string;
+
+  constructor(type: string, name: string, content: string) {
+    this.type = type;
+    this.name = name;
+    this.content = content;
+  }
+}
