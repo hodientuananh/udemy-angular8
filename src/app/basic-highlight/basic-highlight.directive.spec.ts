941f6932f9fbaae1b8994cb08d0aba2c301be690
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-24 13:47:56 +0700

    finish directives

---
+++ src/app/basic-highlight/basic-highlight.directive.spec.ts
@@ -0,0 +1,9 @@
+import { BasicHighlightDirective } from './basic-highlight.directive';
+
+describe('BasicHighlightDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new BasicHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor = this.defaultColor;
+
+  @HostListener('mouseenter', ) mouseEnter( eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave', ) mouseLeave( eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+}
