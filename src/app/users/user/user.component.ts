a838d2e6cb5edb9ff2b850c4bf353fd9abdb0993
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-25 13:57:44 +0700

    pass query params and fragment

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubcription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.paramsSubcription = this.route.params.subscribe(
+      (param: Params) => {
+        this.user.id = param['id'];
+        this.user.name = param['name'];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubcription.unsubscribe();
   }
 
 }
