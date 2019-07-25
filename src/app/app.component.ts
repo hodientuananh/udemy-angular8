ad57a535e6a51c5dc1626ce7c2a03319e6669300
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-25 09:49:11 +0700

    finish service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'udemy-angular8';
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountService: AccountsService) {}
+
+  ngOnInit(): void {
+    this.accounts = this.accountService.accounts;
+  }
 }
