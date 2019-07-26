01ff5fd8a5eace2843037b5bf97f8d90853247c5
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-26 10:43:13 +0700

    add deactive guard to edit route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from './app-routing.module';
+import {AuthService} from './auth.service';
+import {AuthGuardService} from './auth-guard.service';
+import {CanDeactiveGuardService} from './servers/edit-server/can-deactive-guard.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,7 @@ import {AppRoutingModule} from './app-routing.module';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuardService, CanDeactiveGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
